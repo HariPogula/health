a36c2efcffc384739820cee6a288eface657c5e7
HariPogula <hariar2.0@outlook.com>
2017-03-19 22:18:22 -0400

    Add files via upload

---
+++ Scripts/customscript.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+    $(".dropdown").hover(
+            function () {
+                $('.dropdown-menu', this).stop(true, true).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");
+            },
+            function () {
+                $('.dropdown-menu', this).stop(true, true).fadeOut("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");
+            });
+});
+
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+
